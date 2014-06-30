b104220b48e32f574c8ec7b53a1d59f5e859eecb
kristof sajdak <kristof.sajdak@gmail.com>
2014-06-30 20:56:50 +0200

    removed spaces

--- provision/provision_fig.sh
+++ provision/provision_fig.sh
@@ -1,7 +1,3 @@
-usermod -a -G docker vagrant ;
-
 apt-get install -q -y --force-yes curl ;
-     
 curl -L https://github.com/orchardup/fig/releases/download/0.4.2/linux > /usr/local/bin/fig ;
-
 chmod +x /usr/local/bin/fig
