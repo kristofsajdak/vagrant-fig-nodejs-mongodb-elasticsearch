9715c366555347d5e5b93ec9987ba645367746d9
kristof sajdak <kristof.sajdak@gmail.com>
2014-06-30 21:54:07 +0200

    added fig up and npm install to provisioning added photos resource and mongoose

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var _ = require('lodash');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://' + process.env.DB_PORT_27017_TCP_ADDR + ':' + process.env.DB_PORT_27017_TCP_PORT +'/test');
+var Photo = mongoose.model('Photo', { name: String });
 
 var app = express();
+app.use(bodyParser());
+app.use(morgan('dev'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send('API server up and running !');
 });
 
+
+app.post('/photos', function (req, res, next) {
+    var body = req.body;
+    var photo = new Photo(body);
+    photo.save(function (err) {
+        if (err) throw err;
+        res.send(photo);
+    });
+});
+
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.send(500, 'Something broke!');
+});
+
+
 app.listen(process.env.PORT);
+
+
+
