b88b4a1bbfba6dd7c81d65a486d3e0911cde8842
kristof sajdak <kristof.sajdak@gmail.com>
2014-06-30 11:26:49 +0200

    added alias to provisioning script

--- provision.sh
+++ provision.sh
@@ -5,4 +5,5 @@ docker run -v /vagrant:/src -w /src -it --rm  agco/nodejs-forever npm prune
 docker run -v /vagrant:/src -w /src -it --rm  agco/nodejs-forever npm install
 docker build -t agco/nodejs-forever /vagrant
 docker run -d -v /vagrant:/src -w /src -it --link db:db -p 8080:8080 -p 5858:5858 -e "PORT=8080" agco/nodejs-forever forever -w -c 'node --debug' app.js
+alias dl='docker ps -l -q'
 
