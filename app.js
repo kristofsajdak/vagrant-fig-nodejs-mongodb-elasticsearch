d332266c70bb908e986e9e9a9561daa571468009
kristof sajdak <kristof.sajdak@gmail.com>
2014-07-21 16:17:37 +0200

    mongodb, es and port env variables are now provisioned through fig rather than docker links to match Heroku style env variables provisioning

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 
 var mongodb_url;
-if(process.env.NODE_ENV == 'sandbox') {
-    mongodb_url = 'mongodb://' + process.env.DB_PORT_27017_TCP_ADDR + ':' + process.env.DB_PORT_27017_TCP_PORT + '/test';
-} else {
-    mongodb_url = process.env.MONGODB_URL;
-}
+mongodb_url = process.env.MONGODB_URL + '/test';
 
 mongoose.connect(mongodb_url);
 
@@ -26,7 +22,7 @@ app.use(function (err, req, res, next) {
 });
 
 
-app.listen(8080);
+app.listen(process.env.PORT);
 
 
 
