55da166ce78db8edcea590e73ed4f9e532a14124
kristof sajdak <kristof.sajdak@gmail.com>
2014-07-31 11:46:53 +0200

    forever now outputs it's err and out logs to a local logs folder

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var _s = require('underscore.string');
+var _ = require('lodash');
 
 var mongodb_url;
 mongodb_url = process.env.MONGODB_URL + '/test';
@@ -13,7 +15,19 @@ app.use(bodyParser());
 app.use(morgan('dev'));
 
 app.get('/', function (req, res) {
-    res.send('API server up and running !');
+    res.send('API server up and running !!!!');
+});
+
+app.get('/dealers', function (req, res) {
+    var facetProps = _.filter(Object.keys(req.query), function (propName) {
+        return (_s.startsWith(propName, 'facet.field'));
+    });
+    var facets = _.map(facetProps,function (facetProp) {
+        return facetProp + '=' + req.query[facetProp];
+    }).join(',');
+
+    res.send('request came in with following facets : ' + facets);
+
 });
 
 app.use(function (err, req, res, next) {
