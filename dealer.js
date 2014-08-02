d1da5e32ec0270d5837796638f53381c66497dfe
kristof sajdak <kristof.sajdak@gmail.com>
2014-08-02 15:03:54 +0200

    implemented /dealers GET commented out facets

--- dealer.js
+++ dealer.js
@@ -1,9 +1,8 @@
-
 var _s = require('underscore.string');
 var _ = require('lodash');
 var uuid = require('node-uuid');
 
-module.exports = function(app, mongoose) {
+module.exports = function (app, mongoose) {
 
     var dealerSchema = new mongoose.Schema({
         _id: { type: String, default: function () {
@@ -23,42 +22,47 @@ module.exports = function(app, mongoose) {
             number: String
         },
         links: {
-            parent: String,
-            bill_to: String,
-            ship_to: String,
-            current_contracts: {type: [String], default: function () {
-                return [];
-            }},
-            "address.country": String,
-            "address.state_province": String,
-            phone_numbers: String,
-            business_hours: String,
-            dealer_misc: String,
-            current_lines_of_business: [String],
-            service_level: String,
-            current_offerings: [String]
+            "address.country": String
         }
     });
 
     var Dealer = mongoose.model('Dealer', dealerSchema);
+
+    var transformDoc = function (doc, ret, options) {
+        ret.id = doc._id;
+        delete ret._id;
+        delete ret.__v;
+    };
+
+    var mapDocs = function (docs) {
+        return _.map(docs, function (doc) {
+            return doc.toObject({transform: doc});
+        });
+    };
 
     app.post('/dealers', function (req, res, next) {
         var dealer = new Dealer(req.body);
         dealer.save(function (err) {
             if (err) return next(err);
         });
-        res.send(dealer.toJSON());
+        var dealerObj = dealer.toObject({transform: transformDoc});
+        res.send(JSON.stringify(dealerObj));
     });
 
     app.get('/dealers', function (req, res) {
-        var facetProps = _.filter(Object.keys(req.query), function (propName) {
-            return (_s.startsWith(propName, 'facet.field'));
+        Dealer.find({}, function (err, docs) {
+            if (err) return next(err);
+            var data = mapDocs(docs);
+            res.send(data);
         });
-        var facets = _.map(facetProps,function (facetProp) {
-            return facetProp + '=' + req.query[facetProp];
-        }).join(',');
+        /* var facetProps = _.filter(Object.keys(req.query), function (propName) {
+         return (_s.startsWith(propName, 'facet.field'));
+         });
+         var facets = _.map(facetProps,function (facetProp) {
+         return facetProp + '=' + req.query[facetProp];
+         }).join(',');
 
-        res.send('request came in with following facets : ' + facets);
+         res.send('request came in with following facets : ' + facets);*/
 
     });
 };
