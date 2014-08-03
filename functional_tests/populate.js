7dadcecd9d9383553092c9bc04ae354f4059cf29
kristof sajdak <kristof.sajdak@gmail.com>
2014-08-03 16:00:48 +0200

    populate script is in a fair state now works with promises

--- functional_tests/populate.js
+++ functional_tests/populate.js
@@ -3,126 +3,109 @@ var Promise = require('bluebird'),
 var _s = require('underscore.string');
 var _ = require('lodash');
 
-var countries = {};
-/*
- function rp(c) {
 
- c.headers = c.headers || {};
- if (!c.headers['accept']) c.headers['accept'] = 'application/vnd.api+json';
- if (!c.headers['content-type']) c.headers['content-type'] = 'application/vnd.api+json';
+var request = Promise.promisify(require('request'));
+var headers = {'content-type': 'application/vnd.api+json'};
 
- return new Promise(function (resolve, reject) {
- request(c, function (error, response, body) {
- if (error) {
- reject(error);
- } else {
- if (response.statusCode !== 200 && response.statusCode !== 201) {
- reject(response);
- } else {
- resolve(response, body);
- }
- }
- });
- });
- }*/
+var do_request = function (path, method, body) {
+    return Promise.resolve()
+        .then(function () {
+            var body_serialized = JSON.stringify(body);
+            return request({uri: uri + path, headers: headers, method: method, body: body_serialized})
+        });
+};
+
+function map_code_with_id(results, map, resource) {
+    var id = JSON.parse(results[0].body)[resource][0].id;
+    var code = JSON.parse(results[0].body)[resource][0].code;
+    map[code] = id;
+}
 
 var uri = 'http://192.168.50.4:8080';
 
-var request = Promise.promisify(require('request'));
 
-var headers = {'content-type': 'application/vnd.api+json'};
+var regions = {};
+var insertRegions = function () {
 
-var body_serialized = JSON.stringify({
-    "regions": [
-        {
-            "code": "NA",
-            "description": "North America"
-        }
-    ]
-});
-
-request({uri: uri + '/regions', headers: headers, method: 'POST', body: body_serialized})
-    .spread(function (response, body) {
-        console.log('bla' + response.body);
-    })
-    .then(function () {
-        var body_serialized = JSON.stringify({
-            "regions": [
-                {
-                    "code": "EU",
-                    "description": "Europe"
-                }
-            ]
-        });
-        return request({uri: uri + '/regions', headers: headers, method: 'POST', body: body_serialized});
-    })
-    .spread(function (response, body) {
-        console.log('ha' + response.body);
+    var eu = do_request('/regions', 'POST', {
+        "regions": [
+            {
+                "code": "EU",
+                "description": "Europe"
+            }
+        ]
     });
 
-/*var do_request = function (path, method, body, handle_response) {
- var serialized_body = body ? JSON.stringify(body) : '';
- return rp({uri: uri + path, method: method, body: serialized_body}).map(function (response) {
- return handle_response(response)
- });
- };
+    var na = do_request('/regions', 'POST', {
+        "regions": [
+            {
+                "code": "NA",
+                "description": "North America"
+            }
+        ]
+    });
 
- do_request('/countries', 'GET', {
- "regions": [
- {
- "code": "NA",
- "description": "North America"
- }
- ]
- }, function (response) {
- console.log(response);
- return 'bla';
- })
- .then(function(result){console.log(result)});*/
+    var sa = do_request('/regions', 'POST', {
+        "regions": [
+            {
+                "code": "SA",
+                "description": "South America"
+            }
+        ]
+    });
+    return Promise.all([ eu, na, sa]).map(function (results) {
+        map_code_with_id(results, regions, 'regions');
+    })
+};
 
-/*
+var countries = {};
+var insertCountries = function () {
+
+    var us = do_request('/countries', 'POST', {
+        "countries": [
+            {
+                "code": "US",
+                "description": "United States",
+                "links" : {
+                    "region": regions["NA"]
+                }
+            }
+        ]
+    });
+
+    var ca = do_request('/countries', 'POST', {
+        "countries": [
+            {
+                "code": "CA",
+                "description": "Canada",
+                "links" : {
+                    "region": regions["NA"]
+                }
+            }
+        ]
+    });
+
+    return Promise.all([ us, ca]).map(function (results) {
+        map_code_with_id(results, countries, 'countries');
+    })
+};
+
+function print() {
+    console.log(JSON.stringify(countries));
+    console.log(JSON.stringify(regions));
+}
+
+Promise.resolve()
+    .then(function () {
+        return insertRegions();
+    })
+    .then(function () {
+        return insertCountries();
+    })
+    .then(function () {
+        print();
+    });
 
 
 
- Promise.resolve()
- .then(rp({uri: 'http://192.168.50.4:8080/regions', method: 'POST', body: JSON.stringify({
- "regions": [
- {
- "id": "1",
- "code": "NA",
- "description": "North America"
- }
- ]
- })}).then(function (response) {
- console.log(response.body)
- }))
- .then(rp({uri: 'http://192.168.50.4:8080/regions', method: 'POST',  body: JSON.stringify({
- "regions": [
- {
- "id": "1",
- "code": "EU",
- "description": "Europe"
- }
- ]
- })}).then(function (response) {
- console.log(response.body)
- }))
- .then(rp({uri: 'http://192.168.50.4:8080/regions', method: 'GET'}).then(function (response) {
- console.log(response.body)
- }))
- .catch(function(error) {
- console.log('catched ' +error);
- });
- */
 
-/*Promise.resolve()
- .then(function () {
- return rp({uri: 'http://192.168.50.4:8080/dealers', method: 'GET'});
- })
- .then(function (result) {
- console.log('result =' + JSON.stringify(result));
- })
- .catch(function (error) {
- console.log('catched' + JSON.stringify(error));
- })
- .done();*/
