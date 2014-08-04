04f3e63fb659b87a25734745450a6bcda7282a65
kristof sajdak <kristof.sajdak@gmail.com>
2014-08-04 18:49:31 +0200

    added es_setup prototyping scraps changed MONGOHQ_URL to MONGODB_URL in fig added alternate key lookup mechanism

--- resources.js
+++ resources.js
@@ -4,6 +4,8 @@ var uuid = require('node-uuid');
 
 module.exports = function (fortune_app, mongoose) {
 
+    var app = fortune_app.router;
+    var request = require('request');
 
     fortune_app.resource('brand', {
         code: String,
@@ -69,27 +71,52 @@ module.exports = function (fortune_app, mongoose) {
         sun_close: String
     });
 
+    app.use('/dealers/code/:code', function (req, res, next) {
+        mongoose.connection.collection('dealers').find({ code: req.params.code}, { _id: 1}).toArray(function (err, ids) {
+            if (ids.length === 0) {
+                return res.status(404).send('Not found');
+            } else {
+                var idsParam = _.pluck(ids, '_id').map(function (objectId) {
+                    return objectId.toString();
+                }).join(',');
+                request('http://127.0.0.1:' + process.env.PORT + '/dealers/' + idsParam, function (error, response, body) {
+                    if (error) {
+                        return next(error);
+                    } else {
+                        return res
+                            .set('content-type', response.headers['content-type'])
+                            .status(response.statusCode)
+                            .send(response.body);
+                    }
+                })
+            }
+        });
+    });
+
+
     fortune_app.resource('dealer', {
         code: String,
         name: String,
         address1: String,
         address2: String,
-        address_country : {ref: 'country', inverse: 'address_country'},
-        address_state_province : {ref: 'state_province', inverse: 'address_state_province'},
+        address_country: {ref: 'country', inverse: 'address_country'},
+        address_state_province: {ref: 'state_province', inverse: 'address_state_province'},
         city: String,
         zip: String,
         phone_area: String,
         phone_number: String,
         /*parent: {ref: 'dealer', inverse: 'parent'},
-        bill_to: {ref: 'dealer', inverse: 'bill_to'},
-        ship_to: {ref: 'dealer', inverse: 'ship_to'},*/
-        current_contracts: [{ref: 'contract', inverse: 'current_contracts'}],
+         bill_to: {ref: 'dealer', inverse: 'bill_to'},
+         ship_to: {ref: 'dealer', inverse: 'ship_to'},*/
+        current_contracts: [
+            {ref: 'contract', inverse: 'current_contracts'}
+        ]
         /*phone_numbers: [{ref: 'phone_number', inverse: 'phone_numbers'}],
-        business_hours: {ref: 'business_hours', inverse: 'business_hours'},
-        dealer_misc:{ref: 'dealer_misc', inverse: 'dealer_misc'},
-        current_lines_of_business: [{ref: 'line_of_business', inverse: 'current_lines_of_business'}],
-        service_level: {ref: 'service_level', inverse: 'service_level'},
-        current_offerings: [{ref: 'offering', inverse: 'current_offerings'}]*/
+         business_hours: {ref: 'business_hours', inverse: 'business_hours'},
+         dealer_misc:{ref: 'dealer_misc', inverse: 'dealer_misc'},
+         current_lines_of_business: [{ref: 'line_of_business', inverse: 'current_lines_of_business'}],
+         service_level: {ref: 'service_level', inverse: 'service_level'},
+         current_offerings: [{ref: 'offering', inverse: 'current_offerings'}]*/
     });
 
 };
